9af6670ebb77be2567ba95f7069574f3d7f37fe1
Your Name <jeyagit-28>
2026-04-16 22:43:52 +0530

    Initial commit with Playwright tests and CI

---
+++ tests/regression/regression-cart.spec.js
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+test('Add item to cart', async ({ page }) => {
+  await page.goto('https://saucedemo.com');
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  
+  await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+  await expect(page.locator('.shopping_cart_badge')).toHaveText('1');
+});
---
+++ tests/smoke/smoke.login.spec.js
@@ -0,0 +1,6 @@
+import { test, expect } from '@playwright/test';
+
+test('Verify login page loads', async ({ page }) => {
+  await page.goto('https://saucedemo.com');
+  await expect(page).toHaveTitle(/Swag Labs/);
+});
